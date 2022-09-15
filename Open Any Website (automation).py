d4e1ca19b6e318568e561d70fd55dcbd4e12f84d
Blue9232 <112566205+Blue9232@users.noreply.github.com>
2022-09-15 19:11:20 +0300

    Update Open Any Website (automation).py

--- Open Any Website (automation).py
+++ Open Any Website (automation).py
@@ -1,4 +1,4 @@
-"Automatically open a Website (any website) using Selenium and Geckodriver"
+"Automatically open a Website (any website) using Selenium, Python & Geckodriver"
 
 from selenium import webdriver
 driver = webdriver.Firefox(executable_path="c:\selenium_browser_drivers\geckodriver.exe")
